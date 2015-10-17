fb057e2bc361021866a44ee6b4aedc2ca120a056
Patrick Motard <motard19@gmail.com>
2015-10-17 16:17:34 +0000

    fixing some odds and ends

--- client/main.js
+++ client/main.js
@@ -5,29 +5,34 @@ $(document).ready(function() {
     vm.numbers = ko.observableArray([0,1,2,3,4,5,6,7,8,9]);
     vm.keyPress = function(data, event){
        
-    }
+    };
     vm.answer = ko.computed(function(){
       
-    }, this)
+    }, vm);
     vm.input = ko.observable("");
     vm.parse = function(element, index, array){
       
     };
     vm.equation = ko.computed(function(){
       var chars = vm.input().split("");
-      chars.forEach(vm.parse)
-    }, self)
+      chars.forEach(vm.parse);
+    }, vm);
 
     vm.addToInput = function(charPressed){
+      var charPressedIsOperator = _.contains(vm.operators(), charPressed);
+      
       var lastChar = _.last(vm.input());
-      var lastCharIsOperator = _.contains(vm.input(), lastChar);
-      var charPressedIsOperator = _.contains(vm.input(), charPressed);
+      
+      if(!lastChar && charPressedIsOperator){vm.input(vm.input()+charPressed)}
+      
+      var lastCharIsOperator = _.contains(vm.operators(), lastChar);
+
       if(charPressedIsOperator && vm.input().length === 0){return;}
+      
       if(lastCharIsOperator && charPressedIsOperator){return;}
       
-      
       vm.input(vm.input() + charPressed);
-    }
+    };
   };
   ko.applyBindings(myViewModel, document.getElementById("calculator"))
 })
